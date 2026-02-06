1397de1f9689f485d34eecaacce164199cc94645
Josh Charles <josh@asleepingturtle.com>
2026-02-05 22:57:48 -0500

    Finished Ch. 4 and implemented db to the app

---
+++ cmd/web/handlers.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	//"html/template"
+	"net/http"
+	"strconv"
+
+	"snippetbox.hardshelllabs.dev/internal/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+	// files := []string{
+	// 	"./ui/html/base.tmpl.html",
+	// 	"./ui/html/partials/nav.tmpl.html",
+	// 	"./ui/html/pages/home.tmpl.html",
+	// }
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// 	return
+	// }
+
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	app.serverError(w, r, err)
+	// }
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form from creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	title := "0 small"
+	content := "0 small\nClimb Mount Fuji,\n But slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	_ "github.com/go-sql-driver/mysql"
+	"log/slog"
+	"net/http"
+	"os"
+	"snippetbox.hardshelllabs.dev/internal/models"
+)
+
+// Application-wide dependencies
+type application struct {
+	logger   *slog.Logger
+	snippets *models.SnippetModel
+}
+
+func main() {
+	// Cmd Args
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:webPassword@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
+
+	// Custom Structure Logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
+	logger.Info("starting server", "addr", *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
