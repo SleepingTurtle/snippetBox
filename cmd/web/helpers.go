161539109aa67f161cf05ae20e2ce566b1931a3d
Josh Charles <josh@asleepingturtle.com>
2026-02-09 03:56:27 -0500

    Chapter 7 WIP - Added createSnippet form and formv validation
    * In-Progress Centralizing Validators

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
-	"runtime/debug"
+	// "runtime/debug"
 	"time"
 )
 
@@ -12,10 +12,11 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
-		trace  = string(debug.Stack())
+		// trace  = string(debug.Stack())
 	)
 
-	app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	// app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
