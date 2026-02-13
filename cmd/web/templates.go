cca84d7db5544cefd975ebcc95b78791aceaeaf5
Josh Charles <josh@asleepingturtle.com>
2026-02-13 04:03:08 -0500

    10.6 - Require Authentication
    * Ran into a non defined rule that stucts can have private values and are case sensitive.
    * Added new middleware in the chain to handle the auth check

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -10,11 +10,12 @@ import (
 
 // Collects all the data that gets injected into the templates
 type templateData struct {
-	CurrentYear int
-	Snippet     models.Snippet
-	Snippets    []models.Snippet
-	Form        any
-	Flash       string
+	CurrentYear     int
+	Snippet         models.Snippet
+	Snippets        []models.Snippet
+	Form            any
+	Flash           string
+	IsAuthenticated bool
 }
 
 func humanDate(t time.Time) string {
