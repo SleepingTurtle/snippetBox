12d2a5ad9dae682b83e40c2c25675979c5bbff53
Josh Charles <josh@asleepingturtle.com>
2026-02-13 18:22:53 -0500

    Chap 11 - added a custom string to the request context to check for authenticated

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,7 +14,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF, app.authenticate)
 
 	// Routes
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
